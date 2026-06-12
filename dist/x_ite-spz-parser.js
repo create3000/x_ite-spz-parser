0dfc5d17ac39496055d9baeaefa02dd104aac5ce
Holger Seelig <holger.seelig@gmail.com>
2026-06-12 10:42:21 +0200

    Parse header.

--- dist/x_ite-spz-parser.js
+++ dist/x_ite-spz-parser.js
@@ -9,6 +9,8 @@ class SPZParser extends X3D .X3DParser
    constructor (scene)
    {
       super (scene);
+
+      this .offset = 0;
    }
 
    getEncoding ()
@@ -20,13 +22,22 @@ class SPZParser extends X3D .X3DParser
    {
       this .input    = input;
       this .dataView = new DataView (input);
+      this .header   = this .parseHeader ();
    }
 
    isValid ()
    {
       // Check magic.
 
-      if (this .dataView .getUint32 (0, true) !== 0x5053474e)
+      if (this .header .magic !== 0x5053474e)
+         return false;
+
+      // Validate header.
+
+      if (this .header .version < 1 || this .header .version > 2)
+         return false;
+
+      if (this .header .shDegree > 3)
          return false;
 
       return true;
@@ -51,6 +62,21 @@ class SPZParser extends X3D .X3DParser
       await this .loadComponents ();
 
       return scene;
+   }
+
+   parseHeader ()
+   {
+      const { dataView } = this;
+
+      return {
+         magic: dataView .getUint32 (this .offset, true),
+         version: dataView .getUint32 (this .offset += 4, true),
+         numPoints: dataView .getUint32 (this .offset += 4, true),
+         shDegree: dataView .getUint8 (this .offset += 4),
+         fractionalBits: dataView .getUint8 (this .offset += 1),
+         flags: dataView .getUint8 (this .offset += 1),
+         reserved: dataView .getUint8 (this .offset += 1),
+      };
    }
 }
 
