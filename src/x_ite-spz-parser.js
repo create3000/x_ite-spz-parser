2b7c778200dcc12adec9feefba23a034ef0f07fe
Holger Seelig <holger.seelig@gmail.com>
2026-06-12 11:09:45 +0200

    Added build scripts.

---
+++ src/x_ite-spz-parser.js
@@ -0,0 +1,91 @@
+const X3D = window [Symbol .for ("X_ITE.X3D")];
+
+/*
+ * Parser
+ */
+
+class SPZParser extends X3D .X3DParser
+{
+   constructor (scene)
+   {
+      super (scene);
+
+      this .offset = 0;
+   }
+
+   getEncoding ()
+   {
+      return "ARRAY_BUFFER";
+   }
+
+   setInput (input)
+   {
+      this .input    = input;
+      this .dataView = new DataView (input);
+      this .header   = this .parseHeader ();
+   }
+
+   isValid ()
+   {
+      const { magic, version, shDegree } = this .header;
+
+      // Check magic.
+
+      if (magic !== 0x5053474e)
+         return false;
+
+      // Validate header.
+
+      if (version < 1 || version > 2)
+         return false;
+
+      if (shDegree > 3)
+         return false;
+
+      return true;
+   }
+
+   parseIntoScene (resolve, reject)
+   {
+      this .spz ()
+         .then (resolve)
+         .catch (reject);
+   }
+
+   async spz ()
+   {
+      console .log (this .header)
+
+      const
+         browser = this .getBrowser (),
+         scene   = this .getScene ();
+
+      scene .setEncoding ("SPZ");
+      scene .setProfile (browser .getProfile ("Interchange"));
+
+      await this .loadComponents ();
+
+      return scene;
+   }
+
+   parseHeader ()
+   {
+      const { dataView } = this;
+
+      const header = {
+         magic: dataView .getUint32 (this .offset, true),
+         version: dataView .getUint32 (this .offset += 4, true),
+         numPoints: dataView .getUint32 (this .offset += 4, true),
+         shDegree: dataView .getUint8 (this .offset += 4),
+         fractionalBits: dataView .getUint8 (this .offset += 1),
+         flags: dataView .getUint8 (this .offset += 1),
+         reserved: dataView .getUint8 (this .offset += 1),
+      };
+
+      this .offset += 1;
+
+      return header;
+   }
+}
+
+X3D .GoldenGate .addParsers (SPZParser);
