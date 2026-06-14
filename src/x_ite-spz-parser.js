7935491223bb7ca81e125ee284860c1cc333f26f
Holger Seelig <holger.seelig@gmail.com>
2026-06-14 08:02:41 +0200

    Use spz-2.1.0 for version 1,2,3.

--- src/x_ite-spz-parser.js
+++ src/x_ite-spz-parser.js
@@ -1,10 +1,7 @@
 import { default as createSpzModule4 } from "./spz-3.0.0/spz.js";
+import { default as createSpzModule123 } from "./spz-2.1.0-adobe.27/spz.js";
 
 const X3D = window [Symbol .for ("X_ITE.X3D")];
-
-const
-   ANTIALIASED_FLAG = 1,
-   COLOR_SCALE      = 0.15;
 
 /*
  * Parser
@@ -17,8 +14,6 @@ class SPZParser extends X3D .X3DParser
    constructor (scene)
    {
       super (scene);
-
-      this .offset = 0;
    }
 
    getEncoding ()
@@ -44,15 +39,8 @@ class SPZParser extends X3D .X3DParser
 
       // Validate header.
 
-      switch (version)
-      {
-         case 1:
-         case 2:
-         case 4:
-            break;
-         default:
-            return false;
-      }
+      if (version < 1 || version > 4)
+         return false;
 
       return true;
    }
@@ -134,6 +122,7 @@ class SPZParser extends X3D .X3DParser
       {
          case 1:
          case 2:
+         case 3:
          {
             const transform = scene .createNode ("Transform");
 
@@ -157,17 +146,17 @@ class SPZParser extends X3D .X3DParser
    {
       const { dataView } = this;
 
-      const header = {
-         magic: dataView .getUint32 (this .offset, true),
-         version: dataView .getUint32 (this .offset += 4, true),
-         numPoints: dataView .getUint32 (this .offset += 4, true),
-         shDegree: dataView .getUint8 (this .offset += 4),
-         fractionalBits: dataView .getUint8 (this .offset += 1),
-         flags: dataView .getUint8 (this .offset += 1),
-         reserved: dataView .getUint8 (this .offset += 1),
-      };
+      let offset = 0;
 
-      this .offset += 1;
+      const header = {
+         magic: dataView .getUint32 (offset, true),
+         version: dataView .getUint32 (offset += 4, true),
+         numPoints: dataView .getUint32 (offset += 4, true),
+         shDegree: dataView .getUint8 (offset += 4),
+         fractionalBits: dataView .getUint8 (offset += 1),
+         flags: dataView .getUint8 (offset += 1),
+         reserved: dataView .getUint8 (offset),
+      };
 
       return header;
    }
@@ -180,7 +169,8 @@ class SPZParser extends X3D .X3DParser
       {
          case 1:
          case 2:
-            return await this .parseSplats12 ();
+         case 3:
+            return await this .parseSplats123 ();
          case 4:
             return await this .parseSplats4 ();
       }
@@ -196,200 +186,14 @@ class SPZParser extends X3D .X3DParser
       return gaussianCloud;
    }
 
-   parseSplats12 ()
+   async parseSplats123 ()
    {
-      const { version, numPoints, shDegree, fractionalBits, flags } = this .header;
-
       const
-         shDimension = this .dimForDegree (shDegree),
-         usesFloat16 = version === 1;
+         SpzModule     = await createSpzModule123 (),
+         data          = new Uint8Array (this .buffer),
+         gaussianCloud = SpzModule .loadSpzFromBuffer (data, { to: SpzModule .CoordinateSystem .RUB });
 
-      // Initialize packed object.
-
-      const packed = {
-         numPoints,
-         shDegree,
-         fractionalBits,
-         antialiased: (flags & ANTIALIASED_FLAG) !== 0,
-         numPositions: numPoints * 3 * (usesFloat16 ? 2 : 3),
-         numRotations: numPoints * 3,
-         numScales: numPoints * 3,
-         numOpacities: numPoints,
-         numColors: numPoints * 3,
-         numShs: numPoints * shDimension * 3,
-      };
-
-      // Read data sections.
-
-      const array = new Uint8Array (this .buffer);
-
-      let currentOffset = this .offset;
-
-      packed .positions = array .subarray (currentOffset, currentOffset += packed .numPositions);
-      packed .opacities = array .subarray (currentOffset, currentOffset += packed .numOpacities);
-      packed .colors    = array .subarray (currentOffset, currentOffset += packed .numColors);
-      packed .scales    = array .subarray (currentOffset, currentOffset += packed .numScales);
-      packed .rotations = array .subarray (currentOffset, currentOffset += packed .numRotations);
-      packed .shs       = array .subarray (currentOffset, currentOffset += packed .numShs);
-
-      // Verify we read the expected amount of data.
-
-      if (currentOffset !== this .buffer .byteLength)
-         throw new Error ("x_ite-spz-parser: incorrect buffer size.");
-
-      return this .unpackSplats (packed);
-   }
-
-   unpackSplats (packed)
-   {
-      const { numPoints, positions, rotations, scales, colors, shs, shDegree } = packed;
-      const shDimension = this .dimForDegree (shDegree);
-      const usesFloat16 = positions .length === numPoints * 3 * 2;
-
-      // Validate sizes.
-
-      if (!this .checkSizes2 (packed, numPoints, shDimension, usesFloat16))
-         throw new Error ("x_ite-spz-parser: incorrect array sizes.");
-
-      const
-         splatPositions = [ ],
-         splatRotations = [ ],
-         splatScales    = [ ],
-         splatAlphas    = [ ],
-         splatColors    = [ ],
-         splatShs       = shs .map (value => this .unquantizeSH (value));
-
-      let halfData;
-
-      if (usesFloat16)
-         halfData = new Uint16Array (positions .buffer, positions .byteOffset, numPoints * 3);
-
-      const fullPrecisionPositionScale = 1 / (1 << packed .fractionalBits);
-
-      for (let i = 0; i < numPoints; ++ i)
-      {
-         // Get splat position.
-
-         if (usesFloat16)
-         {
-            // Decode legacy float16 format.
-
-            for (let j = 0; j < 3; ++ j)
-               splatPositions .push (this .halfToFloat (halfData [i * 3 + j]));
-         }
-         else
-         {
-            // Decode 24-bit fixed point coordinates.
-
-            for (let j = 0; j < 3; ++ j)
-            {
-               const base = i * 9 + j * 3;
-
-               let fixed32 = positions [base];
-
-               fixed32 |= positions [base + 1] << 8;
-               fixed32 |= positions [base + 2] << 16;
-               fixed32 |= (fixed32 & 0x800000) ? 0xff000000 : 0;
-
-               splatPositions .push (fixed32 * fullPrecisionPositionScale);
-            }
-         }
-
-         // Get splat scale.
-
-         for (let j = 0; j < 3; ++ j)
-            splatScales .push (scales [i * 3 + j] / 16 - 10);
-
-         // Get splat rotation.
-
-         const r = rotations .subarray (i * 3, i * 3 + 3);
-
-         const xyz = [
-            r [0] / 127.5 - 1,
-            r [1] / 127.5 - 1,
-            r [2] / 127.5 - 1,
-         ];
-
-         splatRotations .push (xyz [0]);
-         splatRotations .push (xyz [1]);
-         splatRotations .push (xyz [2]);
-
-         const squaredNorm = Math .hypot (xyz [0], xyz [1], xyz [2]);
-
-         splatRotations .push (Math .sqrt (Math .max (0, 1 - squaredNorm)));
-
-         // Get splat opacity.
-
-         splatAlphas .push (this .invSigmoid (packed .opacities [i] / 255));
-
-         // Get splat color.
-
-         for (let j = 0; j < 3; ++ j)
-            splatColors .push (((colors [i * 3 + j] / 255) - 0.5) / COLOR_SCALE);
-      }
-
-      return {
-         numPoints,
-         positions: splatPositions,
-         rotations: splatRotations,
-         scales: splatScales,
-         alphas: splatAlphas,
-         colors: splatColors,
-         sh: splatShs,
-         shDegree,
-      };
-   }
-
-   invSigmoid (x)
-   {
-      return Math .log (x / (1 - x));
-   }
-
-   // Helper function to check sizes (matching C++ checkSizes function)
-   checkSizes2 (packed, numPoints, shDimension, usesFloat16)
-   {
-      if (packed .positions .length !== numPoints * 3 * (usesFloat16 ? 2 : 3))
-         return false;
-
-      if (packed .scales .length !== numPoints * 3)
-         return false;
-
-      if (packed .rotations .length !== numPoints * 3)
-         return false;
-
-      if (packed .opacities .length !== numPoints)
-         return false;
-
-      if (packed .colors .length !== numPoints * 3)
-         return false;
-
-      if (packed .shs .length !== numPoints * shDimension * 3)
-         return false;
-
-      return true;
-   }
-
-   halfToFloat (h)
-   {
-      const sgn = (h >> 15) & 0x1;
-      const exponent = (h >> 10) & 0x1f;
-      const mantissa = h & 0x3ff;
-
-      const signMul = sgn === 1 ? -1 : 1;
-      if (exponent === 0) {
-         return signMul * Math.pow(2, -14) * mantissa / 1024;
-      }
-
-      if (exponent === 31) {
-         return mantissa !== 0 ? NaN : signMul * Infinity;
-      }
-
-      return signMul * Math.pow(2, exponent - 15) * (1 + mantissa / 1024);
-   }
-
-   unquantizeSH (x)
-   {
-      return (x - 128) / 128;
+      return gaussianCloud;
    }
 
    dimForDegree (degree)
