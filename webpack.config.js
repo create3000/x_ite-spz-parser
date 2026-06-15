b9fe3dd8e60803a0cb9f7fcea2273daa19ac3b07
Holger Seelig <holger.seelig@gmail.com>
2026-06-15 17:25:43 +0200

    Generate entries.

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,9 @@ module .exports = async () =>
    const targets = [ ];
 
    targets .push ({
-      entry: {
-         [`${entries [0]}`]: `./src/${entries [0]}.js`,
-         [`${entries [1]}`]: `./src/${entries [1]}.js`,
-      },
+      entry: Object .fromEntries (entries .map (entry => [
+         `${entry}`, `./src/${entry}.js`,
+      ])),
       output: {
          path: path .resolve (__dirname, "dist"),
          filename: "[name].js",
