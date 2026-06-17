2790fc87e4c0cabde1728b3b2c44b8936fe6935e
Holger Seelig <holger.seelig@gmail.com>
2026-06-17 09:26:36 +0200

    Limit extensions to 0.

--- src/x_ite-spz-parser-123.js
+++ src/x_ite-spz-parser-123.js
@@ -51,5 +51,5 @@ for (const canvas of canvases)
 {
    const { element } = X3D .getBrowser (canvas);
 
-   element .setAttribute ("extensions", (element .getAttribute ("extensions")|0) - 1);
+   element .setAttribute ("extensions", Math .max ((element .getAttribute ("extensions")|0) - 1, 0));
 }
