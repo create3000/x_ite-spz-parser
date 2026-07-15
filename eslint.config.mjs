f2512304094db09a24481e4391b7e1ed2bf6868e
Holger Seelig <holger.seelig@gmail.com>
2026-07-15 17:39:31 +0200

    Configure eslint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
+import js               from "@eslint/js";
+import globals          from "globals";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+export default defineConfig ([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions:
+    {
+      globals:
+      {
+        ... globals .browser,
+        ... globals .node,
+      }
+    },
+    rules:
+    {
+      "no-async-promise-executor": "off",
+      "no-empty": ["error", { "allowEmptyCatch": true }],
+      "semi": "error",
+    },
+  },
 ]);
